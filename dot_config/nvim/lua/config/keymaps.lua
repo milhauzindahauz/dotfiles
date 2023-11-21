7b7b3c0df78d24fe3e45bb125e884a663dd25d6f
pskorepa <petr.skorepa@dhl.com>
2023-11-21 11:33:16 +0100

    add keymaps for the cycling tab as intellij

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<M-Left>", ":tabprev<Return>", opts)
+vim.keymap.set("n", "<M-Right>", ":tabnext<Return>", opts)
